a4a99d9f994a67fdbe4f80ee77b93eaeece0a5e7
Mohsen <moohamdiann@gmail.com>
2025-04-13 00:05:36 +0330

    create class for client and atach header for message length on data

--- classes.py
+++ classes.py
@@ -21,13 +21,22 @@ class Server(socket.socket):
         connected = True
         
         while connected:
-            # msg_length = int(conn.recv(self.header_length))
-            msg = conn.recv(1024).decode()
+            header = conn.recv(self.header_length)
+            msg_length = self.read_header(header)
+            msg = conn.recv(msg_length).decode()
             if not msg:
                 connected = False
                 break
             msg_handler(msg, addr)
     
+    
+    def read_header(self, header:str) -> int:
+        msg_length = header.strip()
+        if msg_length:
+            msg_length = int(msg_length.decode())
+        else: 
+            msg_length = 0
+        return msg_length
     
     def start(self):
         self.listen()
@@ -37,7 +46,7 @@ class Server(socket.socket):
             thread = threading.Thread(target=self._client_handler, args=(conn, addr))
             thread.start()
             print("INSIDE WHILE")
-        
+    
     def print_msg(self, msg, addr):
         print(f'{addr} --> "{msg}"')
             
@@ -45,4 +54,39 @@ class Server(socket.socket):
         
     def sendmsg(self, msg:str):
         self.sendall(msg.encode())
+        
+        
+
+class Client(socket.socket):
+    
+    def __init__(self, host:str='127.0.0.1', port:int=65432, header_length=64, msg_handler_callback=None):
+        super().__init__(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
+        self.ADDR = (host, port)
+        
+        self.header_length = header_length
+        self.msg_handler_callback = msg_handler_callback
+        
+    
+    def start(self):
+            
+        connected = True
+        while connected:
+            msg = self.get_input()
+            data = self.atach_header(msg)
+            self.sendall(data)
+    
+    def get_input(self):
+        inp = input()
+        
+        return inp
+    
+    def atach_header(self, msg:str):
+        msg_length = len(msg)
+        
+        header = str(msg_length).ljust(self.header_length, ' ')
+        return (header + msg).encode()
+        
+        
         
