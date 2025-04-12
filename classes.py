6d0df457ce6717be7efa0ee1b13f6d18ac76413b
Mohsen <moohamdiann@gmail.com>
2025-04-12 18:06:59 +0330

    create a class just for server managment

--- classes.py
+++ classes.py
@@ -1,22 +1,47 @@
 import socket
+import threading
 
-
-class Connection(socket.socket):
-    def __init__(self, server:bool=False, host:str='127.0.0.1', port:int=65432):
+class Server(socket.socket):
+    def __init__(self, host:str='127.0.0.1', port:int=65432, header_length=64, msg_handler_callback=None):
         super().__init__(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
         
-        if server:
-            self.bind((host, port))
+        self.header_length = header_length
+        
+        self.msg_handler_callback = msg_handler_callback
+        
+        self.bind((host, port))
 
         
-    def receive(self, bufsize:int=1024):
-        data = self.recv(bufsize)
-        if data:
-            return data.decode()
-        else:
-            return data
+    def _client_handler(self, conn:socket, addr):
+        
+        msg_handler = self.msg_handler_callback if self.msg_handler_callback else self.print_msg
+        
+        connected = True
+        
+        while connected:
+            # msg_length = int(conn.recv(self.header_length))
+            msg = conn.recv(1024).decode()
+            if not msg:
+                connected = False
+                break
+            msg_handler(msg, addr)
+    
+    
+    def start(self):
+        self.listen()
+        
+        while True:
+            conn, addr = self.accept()
+            thread = threading.Thread(target=self._client_handler, args=(conn, addr))
+            thread.start()
+            print("INSIDE WHILE")
+        
+    def print_msg(self, msg, addr):
+        print(f'{addr} --> "{msg}"')
+            
+            
         
     def sendmsg(self, msg:str):
         self.sendall(msg.encode())
--- server.py
+++ server.py
@@ -1,31 +1,17 @@
 import socket
-
+from classes import Server
 
 HOST = '127.0.0.1'
 PORT = 65432
 ADDR = (HOST, PORT)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = Server(host=HOST, port=PORT)
 
-
-server.bind(ADDR)
-
-
-server.listen()
+server.start()
 
 print(f'Server is listening on host {HOST} and port {PORT}...')
-
-conn, addr = server.accept()
 
 print(f'A connection was accepted by address {addr}')
 
 
-while True:
-    data = conn.recv(1024)
-    if data.decode() == '.exitexit':
-        print("The connection was closed by client")
-        break
-    
-    print(data.decode())
-    conn.sendall('message was received'.encode())
 
