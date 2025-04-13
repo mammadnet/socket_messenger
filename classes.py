0fcb945a843a51f54befb5c93dad6e900fd558b9
Mohsen <moohamdiann@gmail.com>
2025-04-13 15:04:34 +0330

    send client message to all other clients

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 
+import json
+
 class Server(socket.socket):
     def __init__(self, host:str='127.0.0.1', port:int=65432, header_length=64, msg_handler_callback=None):
         super().__init__(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,6 +14,8 @@ class Server(socket.socket):
         self.msg_handler_callback = msg_handler_callback
         
         self.bind((host, port))
+        
+        self.conections:set[socket.socket] = set()
 
         
     def _client_handler(self, conn:socket, addr):
@@ -22,12 +26,15 @@ class Server(socket.socket):
         
         while connected:
             header = conn.recv(self.header_length)
-            msg_length = self.read_header(header)
-            msg = conn.recv(msg_length).decode()
-            if not msg:
+            data_length = self.read_header(header)
+            received_msg  = conn.recv(data_length).decode()
+            data = self.decorate_msg(addr, received_msg)
+            if not received_msg:
                 connected = False
                 break
-            msg_handler(msg, addr)
+            
+            self.send_to_all_client(data)
+            msg_handler(data, addr)
     
     
     def read_header(self, header:str) -> int:
@@ -38,11 +45,42 @@ class Server(socket.socket):
             msg_length = 0
         return msg_length
     
+    
+    def atach_header(self, msg:str):
+        msg_length = len(msg)
+        
+        header = str(msg_length).ljust(self.header_length, ' ')
+        return (header + msg).encode()
+    
+    def atach_sender_addr(self, addr, msg):
+        data = {
+            'host':addr[0],
+            'port':addr[1]
+        }
+        
+        data = json.dumps(data)
+        
+        return data+msg
+    
+    def decorate_msg(self, addr, msg):
+        data_with_addr = self.atach_sender_addr(addr, msg)
+        data_with_length = self.atach_header(data_with_addr)
+        
+        return data_with_length
+        
+        
+    def send_to_all_client(self, data):
+        
+        for client in self.conections:
+            client.sendall(data)
+            
+            
     def start(self):
         self.listen()
         
         while True:
             conn, addr = self.accept()
+            self.conections.add(conn)
             thread = threading.Thread(target=self._client_handler, args=(conn, addr))
             thread.start()
             print("INSIDE WHILE")
