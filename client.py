e77fbdb9d4e58e656ec487fd85a6b7173c3e7b08
Mohsen <moohamdiann@gmail.com>
2025-04-15 17:40:03 +0330

    get data from terminal

--- client.py
+++ client.py
@@ -10,12 +10,25 @@ PORT = int(os.getenv('PORT'))
 ADDR = (HOST, PORT)
 
 
-client = Client()
 
-client.connect(ADDR)
+username = input("Enter your username-->")
+client = Client(HOST, PORT)
+client.initializer(username)
+
+while True:
+    inp = input("---->")
+    if inp == '.exitexit':
+        client.close_connection()
+        break
+    data = {
+        'type':'msg',
+        'msg':inp
+    }
+    client.send_data(data)
+
+
 
 print("--> You can close the program by enter '.exitexit' command <--")
 
-client.start()
 
 
